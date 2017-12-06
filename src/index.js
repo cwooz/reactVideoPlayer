bac257058c1e83e84c8a4207702d84716eacc10a
cwooz <woosley.christopher@gmail.com>
2017-12-06 13:06:23 -0500

    components folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //create new component
-
-const App = function() {
+// const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
 //show component on webpage
-
+ReactDOM.render(<App />, document.querySelector('.container'));
