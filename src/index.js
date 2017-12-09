4a83841814a37e68b90090ff2f3a669bd8b378f2
cwooz <woosley.christopher@gmail.com>
2017-12-08 20:41:38 -0500

    controlled components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyB8sPzLRQDO2Zti7_j_0fPEitMihDfaNpI';
 
-//create new component
-// const App = function() {
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );  
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, tem: 'surfboards'}, (videos) => {
+      // this.setState({ videos: videos })
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );  
+  }
 }
 
-//show component on webpage
 ReactDOM.render(<App />, document.querySelector('.container'));
